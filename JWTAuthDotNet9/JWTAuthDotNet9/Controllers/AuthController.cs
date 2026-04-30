abd6f250cf9e10babb14322ade534f903eabaa07
Abhishek Waghmare <abhishekwaghmare1996@gmail.com>
2026-04-30 15:54:01 +0530

    Introduced scalar api

--- JWTAuthDotNet9/JWTAuthDotNet9/Controllers/AuthController.cs
+++ JWTAuthDotNet9/JWTAuthDotNet9/Controllers/AuthController.cs
@@ -1,6 +1,7 @@
 ﻿using JWTAuthDotNet9.Entities;
 using JWTAuthDotNet9.Models;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace JWTAuthDotNet9.Controllers
@@ -9,11 +10,19 @@ namespace JWTAuthDotNet9.Controllers
     [ApiController]
     public class AuthController : ControllerBase
     {
+        public static User user = new();
+
         [HttpPost("register")]
 
         public ActionResult<User> Register(UserDto request)
         {
-            var hashedPassword = 
+            var hashedPassword = new PasswordHasher<User>()
+                .HashPassword(user,request.Password);
+
+            user.Username = request.Username;
+            user.PasswordHash = hashedPassword;
+
+            return Ok(user);
         }
     }
 }
